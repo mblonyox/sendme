d000bb950618f952674ff55e2156b95283eaf9f2
Sukirno <mblonyox@gmail.com>
2024-03-30 15:13:16 +0700

    fix: move description to about page

--- routes/about.tsx
+++ routes/about.tsx
@@ -4,7 +4,26 @@ export default defineRoute(() => {
   return (
     <>
       <h2>About</h2>
-      <p>// TODO: Write about the SendMe.</p>
+      <section>
+        <h3>What is SendMe?</h3>
+        <p>
+          SendMe is a tool that let you send or receive file directly between
+          devices using peer-to-peer (P2P) utilizing WebRTC technology. Files
+          are encrypted using key that generated on web browser.
+        </p>
+        <p>
+          It tooks inspiration from the{" "}
+          <a
+            href="https://sendfiles.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            sendfiles.dev
+          </a>{" "}
+          that using Amazon Web Service (AWS) architecture. Instead it uses Deno
+          Deploy architecture.
+        </p>
+      </section>
     </>
   );
 });
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,26 +5,6 @@ export default defineRoute(() => {
   return (
     <>
       <section>
-        <h2>What is SendMe?</h2>
-        <p>
-          SendMe is a tool that let you send or receive file directly between
-          devices using peer-to-peer (P2P) utilizing WebRTC technology. Files
-          are encrypted using key that generated on web browser.
-        </p>
-        <p>
-          It tooks inspiration from the{" "}
-          <a
-            href="https://sendfiles.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            sendfiles.dev
-          </a>{" "}
-          that using Amazon Web Service (AWS) architecture. Instead it uses Deno
-          Deploy architecture.
-        </p>
-      </section>
-      <section>
         <Client />
       </section>
     </>
